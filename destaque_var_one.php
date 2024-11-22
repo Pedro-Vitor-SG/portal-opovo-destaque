305aebfba6af0cadb9fc98a73e440a3c73a8acbb
Pedro Vitor <pedro.vitor@opovodigital.com>
2024-11-22 10:56:04 -0300

    feat: first commit

---
+++ destaque_var_one.php
@@ -0,0 +1,109 @@
+<section class="emphasis emphasis_var_one mb-5">
+    <h1 class="fw-bold mb-3">VARIAÇÃO 1</h1>
+    <div class="d-lg-flex box-bigger-matter">
+        <div class="box-news col-lg-8 d-md-flex pr-lg-12 pb-lg-32 pb-md-24">
+            <div class="col-md-6 pr-md-12 mb-md-0 mb-12">
+                <a href="">
+                    <figure style="height: 288px; background:  #C62E2E; border-radius: 10px;" class="m-0">
+                        <img src="" alt="">
+                    </figure>
+                </a>
+            </div>
+
+            <div class="col-md-6 pl-md-12">
+                <a href="">
+                    <span class="topper fw-bold">publieditorial</span>
+                    <p class="fw-bold text-truncate-4 text-matter-emphasis mb-12 mb-md-0">Entidades públicas denunciam ato de vandalização e LGBTfobia em Sobral</p>
+                </a>
+                <div>
+                    <a href="" class="text-matter-related d-flex position-relative pb-4 mb-12 mb-md-0">
+                        <span class="pl-16 text-truncate-2">Entidades públicas denunciam ato de vandalização e LGBTfobia em SobralEntidades públicas denunciam ato de vandalização e LGBTfobia em Sobral</span>
+                    </a>
+                    <a href="" class="text-matter-related d-flex position-relative mb-16 mb-md-0">   
+                        <span class="pl-16 text-truncate-2">Vice-líder do governo diz que Planalto falhou ao não conseguir maioria em CPIEntidades públicas denunciam ato de vandalização e LGBTfobia em Sobral</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 d-md-flex d-lg-block pl-lg-12 pt-md-24 pt-lg-0">
+            <div class="box-news col-md-6 col-lg-12 d-flex pb-lg-32 pr-md-12 pr-lg-0 mb-lg-32 pt-16 pb-16 pt-md-0 pb-md-24">
+                <div class="col-6 pr-12">
+                    <a href="">
+                        <figure style="height: 112px; background:  #C62E2E; border-radius: 10px;" class="m-0">
+                            <img src="" alt="">
+                        </figure>
+                    </a>
+                </div>
+                <div class="col-md-6 pl-12">
+                    <a href="">
+                        <span class="topper  fw-bold">publieditorial</span>
+                        <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                    </a>
+                </div>
+            </div>
+
+            <div class="box-news col-md-6 col-lg-12 d-flex pl-md-12 pl-lg-0 pt-16 pb-16 pt-md-0 pb-md-24">
+                <div class="col-6 pr-12">
+                    <a href="">
+                        <figure style="height: 112px; background:  #C62E2E; border-radius: 10px;" class="m-0">
+                            <img src="" alt="">
+                        </figure>
+                    </a>
+                </div>
+                <div class="col-md-6 pl-12">
+                    <a href="">
+                        <span class="topper  fw-bold">publieditorial</span>
+                        <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-12 d-md-flex pt-lg-32 pt-md-24 second-row">
+        <div class="box-news col-lg-4 col-md-6 d-flex pr-md-12  pt-16 pb-16 pt-md-0 pb-md-24">
+            <div class="col-6 pr-12">
+                <a href="">
+                    <figure style="height: 112px; background:  #C62E2E; border-radius: 10px;" class="m-0">
+                        <img src="" alt="">
+                    </figure>
+                </a>
+            </div>
+            <div class="col-md-6 pl-12">
+                <a href="">
+                    <span class="topper  fw-bold">publieditorial</span>
+                    <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                </a>
+            </div>
+        </div>
+        <div class="box-news col-lg-4 col-md-6 d-flex pl-md-12 pr-lg-12 pb-16 pt-16 pt-md-0 pb-md-24">
+            <div class="col-6 pr-12">
+                <a href="">
+                    <figure style="height: 112px; background:  #C62E2E; border-radius: 10px;" class="m-0">
+                        <img src="" alt="">
+                    </figure>
+                </a>
+            </div>
+            <div class="col-md-6 pl-12">
+                <a href="">
+                    <span class="topper  fw-bold">publieditorial</span>
+                    <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                </a>
+            </div>
+        </div>
+        <div class="box-news col-lg-4 d-lg-flex d-none pl-md-12">
+            <div class="col-6 pr-12">
+                <a href="">
+                    <figure style="height: 112px; background: #C62E2E; border-radius: 10px;" class="m-0">
+                        <img src="" alt="">
+                    </figure>
+                </a>
+            </div>
+            <div class="col-md-6 pl-12">
+                <a href="">
+                    <span class="topper topper-publi fw-bold">publieditorial</span>
+                    <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ destaque_var_two.php
@@ -0,0 +1,142 @@
+<section class="emphasis emphasis-two">
+    <h1 class="fw-bold mb-3">VARIAÇÃO 3</h1>
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-12">
+            <div class=" box-news pb-lg-32 mb-lg-32 pb-md-24 mb-md-24 mb-16">
+                <div class="col-12">
+                    <a href="">
+                        <span class="topper fw-bold">Politica</span>
+                        <p class="fw-bold text-truncate-4 text-matter-emphasis mb-12 mb-md-0 mb-lg-4">Entidades públicas denunciam ato de vandalização e LGBTfobia em Sobral</p>
+                    </a>
+                </div>
+                <div class="col-md-12 ">
+                    <a href="" class="text-matter-related d-flex position-relative pb-4 mb-12 mb-md-0">
+                        <span class="pl-16 text-truncate-2 mb-lg-4">Entidades públicas denunciam ato de vandalização e LGBTfobia em SobralEntidades públicas denunciam ato de vandalização e LGBTfobia em Sobral</span>
+                    </a>
+                    <a href="" class="text-matter-related d-flex position-relative mb-16 mb-md-0">
+                        <span class="pl-16 text-truncate-2">Vice-líder do governo diz que Planalto falhou ao não conseguir maioria em CPIEntidades públicas denunciam ato de vandalização e LGBTfobia em Sobral</span>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-lg-4 col-md-6 col-12">
+            <div class="d-flex  box-news pb-lg-32 mb-lg-32 pb-md-24 mb-md-24 pb-16 mb-16">
+                <div class="col-6 pr-12">
+                    <a href="">
+                        <figure style="height: 112px; background: #65B741; border-radius: 10px;" class="m-0">
+                            <img src="" alt="">
+                        </figure>
+                    </a>
+                </div>
+                <div class="col-md-6 pl-12">
+                    <a href="">
+                        <span class="topper fw-bold">publieditorial</span>
+                        <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                    </a>
+                </div>
+            </div>
+            <div class="d-flex  box-news pb-lg-32 mb-lg-32 pb-md-24 mb-md-24 pb-16 mb-16">
+                <div class="col-6 pr-12">
+                    <a href="">
+                        <figure style="height: 112px; background:  #65B741; border-radius: 10px;" class="m-0">
+                            <img src="" alt="">
+                        </figure>
+                    </a>
+                </div>
+                <div class="col-md-6 pl-12">
+                    <a href="">
+                        <span class="topper fw-bold">publieditorial</span>
+                        <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-12  d-md-flex gap-4 d-lg-block 
+">
+            <div class="d-flex  box-news pb-lg-32 mb-lg-32 pb-md-24 mb-md-24 pb-16 mb-16">
+                <div class="col-6 pr-12">
+                    <a href="">
+                        <figure style="height: 112px; background:  #65B741; border-radius: 10px;" class="m-0">
+                            <img src="" alt="">
+                        </figure>
+                    </a>
+                </div>
+                <div class="col-md-6 pl-12">
+                    <a href="">
+                        <span class="topper  fw-bold">publieditorial</span>
+                        <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                    </a>
+                </div>
+            </div>
+            <div class="d-flex  box-news pb-lg-32 mb-lg-32 pb-md-24 mb-md-24 pb-16 mb-16">
+                <div class="col-6 pr-12">
+                    <a href="">
+                        <figure style="height: 112px; background:  #65B741; border-radius: 10px;" class="m-0">
+                            <img src="" alt="">
+                        </figure>
+                    </a>
+                </div>
+                <div class="col-md-6 pl-12">
+                    <a href="">
+                        <span class="topper  fw-bold">publieditorial</span>
+                        <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row second-row">
+        <div class="col-lg-4 col-md-6 col-12">
+            <div class="d-flex  box-news pb-lg-32 mb-lg-32 pb-md-24 mb-md-24 pb-16 mb-16">
+                <div class="col-6 pr-12">
+                    <a href="">
+                        <figure style="height: 112px; background:  #65B741; border-radius: 10px;" class="m-0">
+                            <img src="" alt="">
+                        </figure>
+                    </a>
+                </div>
+                <div class="col-md-6 pl-12">
+                    <a href="">
+                        <span class="topper fw-bold">publieditorial</span>
+                        <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-12">
+            <div class="d-flex  box-news pb-lg-32 mb-lg-32 pb-md-24 mb-md-24 pb-16 mb-16 border-0">
+                <div class="col-6 pr-12">
+                    <a href="">
+                        <figure style="height: 112px; background:  #65B741; border-radius: 10px;" class="m-0">
+                            <img src="" alt="">
+                        </figure>
+                    </a>
+                </div>
+                <div class="col-md-6 pl-12">
+                    <a href="">
+                        <span class="topper topper-publi fw-bold">publieditorial</span>
+                        <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-12 d-none d-lg-block">
+            <div class="d-flex  box-news pb-lg-32 mb-lg-32">
+                <div class="col-6 pr-12">
+                    <a href="">
+                        <figure style="height: 112px; background: #65B741; border-radius: 10px;" class="m-0">
+                            <img src="" alt="">
+                        </figure>
+                    </a>
+                </div>
+                <div class="col-md-6 pl-12">
+                    <a href="">
+                        <span class="topper topper-publi fw-bold">publieditorial</span>
+                        <p class="fw-bold text-truncate-4 m-0">Caixa deve investir R$ 93 bilhões em crédito imobiliário ainda em 2021</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
